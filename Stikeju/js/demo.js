095b8e09ff2c27096953e24494931212fb657f36
Universitas Virtual Terbuka <raviyanto@gmail.com>
2015-05-04 00:47:45 +0700

    Update demo.js

--- Stikeju/js/demo.js
+++ Stikeju/js/demo.js
@@ -21,8 +21,8 @@ $(function () {
         url: 'https://api.flickr.com/services/rest/',
         data: {
             format: 'json'
-            method: 'flickr.photos.getRecent',
-            api_key: 'cfa1efa4342aab149bcc563094718521' // jshint ignore:line
+            method: 'flickr.interestingness.getList',
+            api_key: '7617adae70159d09ba78cfec73c13be3' // jshint ignore:line
             
         },
         dataType: 'jsonp',
