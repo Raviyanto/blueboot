247bd6369e7feb8d7bc7a45f6ee927ea78b71aa5
Universitas Virtual Terbuka <raviyanto@gmail.com>
2015-05-11 23:19:28 +0700

    Update stikeju.js

--- js/stikeju.js
+++ js/stikeju.js
@@ -1,17 +1,15 @@
-/* Galeri Keju */
 $(function () {
     'use strict';
-    
+
+    // Load demo images from flickr:
     $.ajax({
         // Flickr API is SSL only:
         // https://code.flickr.net/2014/04/30/flickr-api-going-ssl-only-on-june-27th-2014/
         url: 'https://api.flickr.com/services/rest/',
         data: {
             format: 'json',
-            method: 'flickr.photosets.getPhotos',
-            api_key: '5675f68c4c45f9c0113d18b386881f2f',
-            photoset_id: '72157650283743964',
-            user_id: '44329575@N02'
+            method: 'flickr.interestingness.getList',
+            api_key: '7617adae70159d09ba78cfec73c13be3' // jshint ignore:line
         },
         dataType: 'jsonp',
         jsonp: 'jsoncallback'
@@ -19,7 +17,7 @@ $(function () {
         var linksContainer = $('#links'),
             baseUrl;
         // Add the demo images as links with thumbnails to the page:
-        $.each(result.photoset.photo, function (index, photo) {
+        $.each(result.photos.photo, function (index, photo) {
             baseUrl = 'https://farm' + photo.farm + '.static.flickr.com/' +
                 photo.server + '/' + photo.id + '_' + photo.secret;
             $('<a/>')
