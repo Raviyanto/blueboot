fbbe253d2686326783653f12ea02f0050fde1613
Universitas Virtual Terbuka <raviyanto@gmail.com>
2015-05-14 07:21:55 +0700

    Update tim.js

--- Stikeju/js/tim.js
+++ Stikeju/js/tim.js
@@ -22,9 +22,8 @@ $(function () {
         data: {
             format: 'json',
             method: 'flickr.photosets.getPhotos',
-            api_key: 'c9387dc465bc9dae704107e43c10258e',
-            photoset_id: '72157650283743964',
-            user_id: '44329575@N02'
+            api_key: 'dbb49a0e2dcc3958834f1b92c072be62',
+            photoset_id: '72157627145038616'
         },
         dataType: 'jsonp',
         jsonp: 'jsoncallback'
